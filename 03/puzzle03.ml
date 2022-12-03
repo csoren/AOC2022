ba57c86b86a0449e53db01fe9a0f87c9c6f9f36e
Carsten Elton Sørensen <cso@rift.dk>
2022-12-03 15:17:19 +0100

    Day 03 improved

--- 03/puzzle03.ml
+++ 03/puzzle03.ml
@@ -8,13 +8,11 @@ let char_to_priority = function
   | 'A'..'Z' as ch -> (int_of_char ch) - (int_of_char 'A') + 27
   | _ -> failwith "Unexpected input"
 
-let string_to_priority_set s =
-  BatString.explode s |> BatList.map char_to_priority |> BatSet.of_list
-
-let common_priorities l =
-  BatList.map (BatList.map string_to_priority_set) l
+let common_items l =
+  BatList.map (BatList.map (BatSet.of_list % String.explode)) l
   |> BatList.map (BatSet.any % BatList.reduce BatSet.intersect)
 
+let sum_priorities = BatList.sum % BatList.map char_to_priority
 
 (* First puzzle *)
 
@@ -25,7 +23,7 @@ let halve_string s =
 let rucksacks = BatList.map halve_string input
 
 let first_puzzle () =
-  Printf.printf "First puzzle, the sum of priorities is %d\n" (common_priorities rucksacks |> List.sum)
+  Printf.printf "First puzzle, the sum of priorities is %d\n" (common_items rucksacks |> sum_priorities)
 
 
 (* Second puzzle *)  
@@ -33,11 +31,10 @@ let first_puzzle () =
 let three_elves = BatList.ntake 3 input
 
 let second_puzzle () =
-  Printf.printf "Second puzzle, the sum of priorities is %d\n" (common_priorities three_elves |> List.sum)
+  Printf.printf "Second puzzle, the sum of priorities is %d\n" (common_items three_elves |> sum_priorities)
 
 
 let () =
   print_newline ();
   first_puzzle ();
-  second_puzzle ();
-  ()
+  second_puzzle ()
