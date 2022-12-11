c713ab3394fc84a3977f68826273a46b410fde81
Carsten Elton Sørensen <cso@rift.dk>
2022-12-11 21:15:35 +0100

    Day 11 part 2

--- 11/puzzle11.ml
+++ 11/puzzle11.ml
@@ -11,9 +11,9 @@ let increase_worry old = function
 | Multiply n -> old * n
 | Plus n -> old + n
 
-let one_monkey m =
+let one_monkey max_worry worry_divide m =
   let one_monkey_item (t, f) n = 
-    let n' = increase_worry n m.operation / 3 in
+    let n' = (increase_worry n m.operation / worry_divide) mod max_worry in
     if n' mod m.divisible_by = 0 then (n' :: t, f)
     else (t, n' :: f)
   in
@@ -21,29 +21,36 @@ let one_monkey m =
 
 let add_items m i = { m with items = m.items @ i }
 
-let one_round monkeys =
+let one_round max_worry worry_divide monkeys =
   for i = 0 to Array.length monkeys - 1 do
     let m = monkeys.(i) in
-    let (t, f) = one_monkey m in
+    let (t, f) = one_monkey max_worry worry_divide m in
     monkeys.(i) <- { m with items = []; inspected = m.inspected + List.length m.items };
     monkeys.(m.when_true) <- add_items monkeys.(m.when_true) t;
     monkeys.(m.when_false) <- add_items monkeys.(m.when_false) f;
   done
 
-let do_rounds n m =
+let do_rounds worry_divide n m =
+  let max_worry = List.map (fun v -> v.divisible_by) m |> List.reduce ( * ) in
+  Printf.printf "Max worry: %d\n" max_worry;
   let m' = Array.of_list m in
   for _ = 1 to n do
-    one_round m'
+    one_round max_worry worry_divide m'
   done;
   Array.to_list m'
   
-let solve m =
-  let m' = do_rounds 20 m in
+let solve_part worry_divide rounds m =
+  let m' = do_rounds worry_divide rounds m in
   let m'' = List.sort (fun m1 m2 -> m2.inspected - m1.inspected) m' in
   List.iter (monkey_to_string %> print_endline) m'';
   let monkey_business = List.take 2 m'' |> List.map (fun m -> m.inspected) |> List.reduce ( * ) in
   Printf.printf "Monkey business: %d\n" monkey_business
 
+let solve m =
+  print_endline "Part 1:";
+  solve_part 3 20 m;
+  print_endline "Part 2:";
+  solve_part 1 10000 m
 
 let () =
   print_newline ();
