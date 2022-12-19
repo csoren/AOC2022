9e64890aaff70f0ccf3e58640a4fe926f32bab8d
Carsten Elton Sørensen <cso@rift.dk>
2022-12-19 16:02:31 +0100

    Day 8 part 2

--- 08/puzzle08.ml
+++ 08/puzzle08.ml
@@ -10,11 +10,6 @@ let char_to_digit ch =
 let forest lines =
   lines |> List.map (String.to_list %> List.map char_to_digit) |> Matrix.of_rows
 
-let all_less_than h l =
-  match l with
-    | [ ] -> true
-    | l -> List.for_all (fun v -> h > v) l
-  
 let get_right m x y = Matrix.sub_row_right_edge m (x + 1) y
 
 let get_left m x y = Matrix.sub_row_left_edge m (x - 1) y
@@ -23,27 +18,48 @@ let get_down m x y = Matrix.sub_column_bottom_edge m x (y + 1)
 
 let get_up m x y = Matrix.sub_column_top_edge m x (y - 1)
   
-let is_visible m x y =
-  let h = m.(x).(y) in
-  let is_visible' l = List.exists (fun f -> f m x y |> all_less_than h) l in
-  [get_left; get_right; get_up; get_down]
-  |> is_visible'
+module Part1 = struct
+  let all_less_than h = function
+    | [ ] -> true
+    | l -> List.for_all ((>) h) l
+    
+  let is_visible m x y =
+    let h = m.(x).(y) in
+    [get_left; get_right; get_up; get_down]
+    |> List.exists (fun f -> f m x y |> all_less_than h)
+  
+  let count_visible m =
+    Matrix.sub_coords m (1, Matrix.width m - 2) (1, Matrix.height m - 2)
+    |> List.count_matching (fun (x,y) -> is_visible m x y)
 
-let count_visible_in_row m y =
-  List.range 1 `To (Matrix.width m - 2)
-  |> List.count_matching (fun x ->
-    is_visible m x y |> tap (Printf.printf "%d,%d=%b\n" x y))
+  let solve m =
+    count_visible m + (Matrix.width m * 2 + 2 * Matrix.height m - 4)
 
-let count_visible m =
-  List.range 1 `To (Matrix.height m - 2)
-  |> List.map (fun y -> count_visible_in_row m y)
-  |> List.sum
+end
 
-let solve lines =
-  let m = forest lines in
-  count_visible m + (Matrix.width m * 2 + 2 * Matrix.height m - 4)
+
+module Part2 = struct
+  let viewing_distance x y h m f =
+    let trees = f m x y in
+    List.findi_opt ((<=) h) trees
+    |> Option.map_default (fst %> (+) 1) (List.length trees)
+
+  let scenic_score m (x, y) =
+    let h = m.(x).(y) in
+    [get_left; get_right; get_up; get_down]
+    |> List.map (viewing_distance x y h m)
+    |> List.fold ( * ) 1
+
+  let solve m =
+    Matrix.sub_coords m (1, Matrix.width m - 2) (1, Matrix.height m - 2)
+    |> List.map (scenic_score m) 
+    |> List.max ~cmp:Int.compare
+
+end
 
 
 let () =
   print_newline ();
-  Printf.printf "Part 1, visible trees: %d\n" (solve input)
+  let m = forest input in
+  Printf.printf "Part 1, visible trees: %d\n" (Part1.solve m);
+  Printf.printf "Part 2, best scenic score: %d\n" (Part2.solve m)
