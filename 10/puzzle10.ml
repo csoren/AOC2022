4f3b58e656fcf70ce064cb2850dbfcaf4d97cbc5
Carsten Elton Sørensen <cso@rift.dk>
2023-01-03 15:18:25 +0100

    Day 10 part 2

--- 10/puzzle10.ml
+++ 10/puzzle10.ml
@@ -19,19 +19,25 @@ let register_contents instructions =
     | Noop -> [ reg ]
     | Addx v -> [ reg; reg + v ]
   in
-  let cycles = List.fold (fun regs instruction -> execute (List.last (List.hd regs)) instruction :: regs) [ [1; 1] ] instructions |> List.rev |> List.flatten in
-  cycles
+  List.fold (fun regs instruction -> execute (List.last (List.hd regs)) instruction :: regs) [ [1; 1] ] instructions
+  |> List.rev |> List.flatten
 
-let interesting_cycles =
-  [ 20; 60; 100; 140; 180; 220 ]
-
-let solve_part1 input =
-  let registers = register_contents input in
-  (* print_endline (List.int_list_to_string registers); *)
+let solve_part1 registers =
+  let interesting_cycles = [ 20; 60; 100; 140; 180; 220 ] in
   let strengths = interesting_cycles |> List.map (fun cycle -> List.at registers cycle * cycle) in
   List.sum strengths
+  
+let x_register_to_crt_line xs =
+  List.mapi (fun index x -> if index >= x - 1 && index <= x + 1 then '#' else '.') xs |> String.implode
+
+let solve_part2 registers =
+  let lines_x_values = List.ntake 40 (List.drop 1 registers) in
+  let lines = lines_x_values |> List.map x_register_to_crt_line in
+  List.iter print_endline lines
 
 let () =
   let instructions = input |> List.map line_to_instruction in
+  let registers = register_contents instructions in
   print_newline ();
-  Printf.printf "Part 1: signal strength: %d\n" (solve_part1 instructions)
+  Printf.printf "Part 1: signal strength: %d\n" (solve_part1 registers);
+  solve_part2 registers
