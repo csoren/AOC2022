d92d0cfdbc00ce4ef9abb03b7ccd0b737325f9ad
Carsten Elton Sørensen <cso@rift.dk>
2023-01-09 21:20:23 +0100

    Day 15 part 2

--- 15/puzzle15.ml
+++ 15/puzzle15.ml
@@ -2,11 +2,14 @@ open Batteries
 open Extensions
 
 let test_input =
-  (10, File.lines_of "test-input" |> List.of_enum)
+  (10, 20, File.lines_of "test-input" |> List.of_enum)
 
 let puzzle_input =
-  (2000000, File.lines_of "puzzle-input" |> List.of_enum)
+  (2000000, 4000000, File.lines_of "puzzle-input" |> List.of_enum)
 
+let tuple2_to_string =
+  Tuple2.to_string Int.to_string Int.to_string
+  
 let string_to_coord s =
   let (x, y) = String.split ~by:"," s in
   let extract_int s prefix = 
@@ -28,22 +31,76 @@ let sensors =
 let sensors_distances sensors =
   List.map (fun (sensor, beacon) -> (sensor, distance sensor beacon)) sensors
 
-let coord_cannot_be_beacon sensors x y = 
-  List.exists (fun (sensor, d) -> distance sensor (x, y) <= d) sensors
+let no_overlap r h =
+  snd r < fst h || fst r > snd h 
 
-let solve_part1 (y, input) =
+let overlaps_right candidate r =
+  (fst candidate) <= (snd r) && (snd candidate) > (snd r)
+
+let overlaps_left candidate r =
+  (fst candidate < fst r) && (snd candidate) >= (fst r)
+
+let rec split_range r rs =
+  match rs with
+  | h :: tail ->
+      if no_overlap r h then
+        split_range r tail
+      else if overlaps_left r h then
+        (fst r, fst h - 1) :: split_range (fst h, snd r) rs
+      else if overlaps_right r h then
+        split_range (snd h + 1, snd r) rs
+      else
+        []
+  | [] -> [ r ] 
+
+let sort_range =
+  List.sort (fun t1 t2 -> Int.compare (fst t1) (fst t2))
+
+let rec add_range rs r =
+  let rs = sort_range rs in
+  split_range r rs @ rs
+
+let range_for_sensor y ((sx, sy), d) =
+  let width = d - (Int.abs @@ sy - y) in
+  if width > 0 then Some (sx - width, sx + width)
+  else None
+
+let ranges_contain l v =
+  List.exists (fun (s, e) -> v >= s && v <= e) l
+
+let beacons_on_row sensors y =
+  List.map snd sensors |> List.filter (snd %> (=) y) |> List.map fst |> List.unique ~eq:(=)
+
+let rec merge_ranges = function
+  | h1 :: h2 :: tail when snd h1 + 1 = fst h2 ->
+      merge_ranges ((fst h1, snd h2) :: tail)
+  | h1 :: tail ->
+      h1 :: (merge_ranges tail)
+  | [] -> []
+
+let ranges_for_row sensors y =
+  List.flat_map_opt (range_for_sensor y) sensors |> List.fold add_range [] |> merge_ranges
+
+let solve_part1 (y, _, input) =
   let sensors = sensors input in
-  let beacons = List.map snd sensors |> List.unique ~eq:(Tuple2.eq (=) (=)) in
-  let coords = List.flat_map (fun (c1, c2) -> [c1; c2]) sensors in
-  let (min_x, max_x) = List.map fst coords |> List.min_max ~cmp:Int.compare in
-  let (min_y, max_y) = List.map snd coords |> List.min_max ~cmp:Int.compare in
-  let sensors = List.map (fun (s, b) -> (s, distance s b)) sensors in
-  Printf.printf "Board size: x %d;%d, y %d;%d\n" min_x max_x min_y max_y;
-  (* let line y = List.range min_x `To max_x |> List.map (fun x -> if coord_cannot_be_beacon sensors beacons x y then '#' else '.') |> String.of_list in
-  print_endline (line y); *)
-  (* List.range min_y `To max_y |> List.map line |> List.iter print_endline; *)
-  (List.range (-1100000) `To 4400000 |> List.count_matching (fun x -> coord_cannot_be_beacon sensors x y)) - (List.count_matching (fun (_,by) -> y = by) beacons)
+  let sensors' = List.map (fun (s, b) -> (s, distance s b)) sensors in
+  let ranges = List.flat_map_opt (range_for_sensor y) sensors' |> List.fold add_range [] in
+  let beacons_contained = List.count_matching (ranges_contain ranges) (beacons_on_row sensors y) in
+  (List.map (fun (start,stop) -> stop - start + 1) ranges |> List.sum) - beacons_contained
+
+let solve_part2 (_, rows, input) =
+  let sensors = sensors_distances @@ sensors input in
+  let (x, y) =
+    List.range 0 `To rows |> List.find_map (fun y ->
+      if Int.modulo y 500000 = 0 then Printf.printf "%d\n" y; flush stdout; 
+      match ranges_for_row sensors y with
+      | r1 :: r2 :: [] -> Some (snd r1 + 1, y)
+      | _ -> None)
+  in
+  Printf.printf "Location %d,%d\n" x y;
+  x * 4000000 + y
 
 let () =
   print_newline ();
-  Printf.printf "Part 1: %d\n" (solve_part1 puzzle_input)
+  Printf.printf "Part 1: %d\n" (solve_part1 puzzle_input);
+  Printf.printf "Part 2: %d\n" (solve_part2 puzzle_input)
